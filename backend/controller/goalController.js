8477d4b519b15c8ed4d29c0b7b44cc41d816d917
Siranchao <scao34@myseneca.ca>
2022-11-23 17:13:41 -0500

    add controller and middleware

---
+++ backend/controller/goalController.js
@@ -0,0 +1,48 @@
+//use npm package 'asyncHandler' replace try(..)catch(..) block 
+const asyncHandler = require('express-async-handler')
+
+
+//@controller: get goals
+//@route GET /api/goals
+//@access private
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: `Get Goals`
+    })
+})
+
+//@controller: set a goal
+//@route POST /api/goals
+//@access private
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add text field')
+    }
+
+    res.status(200).json({
+        message: `Create a Goal`
+    })
+})
+
+//@controller: update a goal
+//@route PUT /api/goals/:id
+//@access private
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: `Update a Goal: ${req.params.id}`
+    })
+})
+
+//@controller: delete a goal
+//@route DELETE /api/goals/:id
+//@access private
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: `Delete a Goal: ${req.params.id}`
+    })
+})
+
+module.exports = {
+    getGoals, setGoal, updateGoal, deleteGoal
+}
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,28 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controller/goalController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        message: `Get Goals`
-    })
-})
+router.get('/', getGoals)
 
-router.post('/', (req, res) => {
-    res.status(200).json({
-        message: `Create a Goal`
-    })
-})
+router.post('/', setGoal)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({
-        message: `Update a Goal: ${req.params.id}`
-    })
-})
+router.put('/:id', updateGoal)
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({
-        message: `Delete a Goal: ${req.params.id}`
-    })
-})
+router.delete('/:id', deleteGoal)
 
 module.exports = router
